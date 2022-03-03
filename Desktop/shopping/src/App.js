0448fdd4abc6b1e3787e9e73b228dc87d30e3fe6
nikhil <nikse8944@gmail.com>
2022-03-03 07:47:01 +0530

    first commit

---
+++ Desktop/shopping/src/App.js
@@ -0,0 +1,18 @@
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
